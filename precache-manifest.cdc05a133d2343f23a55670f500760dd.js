b6c2dfb24c79bf7e0dda27789a7cc75976fe5d2f
Naina Gupta <naina24gupta@gmail.com>
2023-09-03 14:06:12 +0530

    Updates

---
+++ precache-manifest.cdc05a133d2343f23a55670f500760dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2fad5fdfa811ac50a6a997e15b6d5d5",
+    "url": "/Weather-app/index.html"
+  },
+  {
+    "revision": "a93cf31909cfb4eecff1",
+    "url": "/Weather-app/static/css/2.741e3868.chunk.css"
+  },
+  {
+    "revision": "21419768d527990c87e0",
+    "url": "/Weather-app/static/css/main.391e91a7.chunk.css"
+  },
+  {
+    "revision": "a93cf31909cfb4eecff1",
+    "url": "/Weather-app/static/js/2.a1cc3747.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-app/static/js/2.a1cc3747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21419768d527990c87e0",
+    "url": "/Weather-app/static/js/main.23d79cba.chunk.js"
+  },
+  {
+    "revision": "50dfc9bb4c4b626ff773",
+    "url": "/Weather-app/static/js/runtime-main.eb3a8be5.js"
+  }
+]);
